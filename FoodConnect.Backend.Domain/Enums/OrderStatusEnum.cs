3b871b2a862b35cfd92daa2e29068bea3a415812
TrungHiu2610 <hieult2610@gmail.com>
2025-10-28 23:03:20 +0700

    init entity

---
+++ FoodConnect.Backend.Domain/Enums/OrderStatusEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodConnect.Backend.Domain.Enums
+{
+    public enum OrderStatusEnum
+    {
+        Pending,
+        Preparing,
+        OutForDelivery,
+        Delivered,
+        Completed,
+        Cancelled,
+        Returned
+    }
+}
