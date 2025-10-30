02ee023605e911778a78b5170744d9d53b58f32c
TrungHiu2610 <hieult2610@gmail.com>
2025-10-30 20:17:18 +0700

    allow user null for guest

---
+++ FoodConnect.Backend.Infrastructure/Migrations/20251030131449_MakeUserIdNullableInCart.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodConnect.Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeUserIdNullableInCart : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Users_UserId",
+                table: "Carts");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Carts",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("2fa4cb76-edee-44e3-95e2-1f841b27929a"),
+                column: "CreatedAtUtc",
+                value: new DateTime(2025, 10, 30, 13, 14, 47, 905, DateTimeKind.Utc).AddTicks(238));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("30d76555-7ff9-41d5-a11e-92a347f725bf"),
+                column: "CreatedAtUtc",
+                value: new DateTime(2025, 10, 30, 13, 14, 47, 905, DateTimeKind.Utc).AddTicks(245));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("4286ba6a-3d40-46be-8539-237190c067b6"),
+                column: "CreatedAtUtc",
+                value: new DateTime(2025, 10, 30, 13, 14, 47, 905, DateTimeKind.Utc).AddTicks(213));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("4cf8d54f-d15c-4f02-b3a4-c40c5206b624"),
+                column: "CreatedAtUtc",
+                value: new DateTime(2025, 10, 30, 13, 14, 47, 905, DateTimeKind.Utc).AddTicks(251));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("ac8e66b0-4fbc-4c97-ad4b-427eab61db1b"),
+                column: "CreatedAtUtc",
+                value: new DateTime(2025, 10, 30, 13, 14, 47, 905, DateTimeKind.Utc).AddTicks(242));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Users_UserId",
+                table: "Carts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Carts_Users_UserId",
+                table: "Carts");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Carts",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("2fa4cb76-edee-44e3-95e2-1f841b27929a"),
+                column: "CreatedAtUtc",
+                value: new DateTime(2025, 10, 30, 5, 38, 37, 265, DateTimeKind.Utc).AddTicks(784));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("30d76555-7ff9-41d5-a11e-92a347f725bf"),
+                column: "CreatedAtUtc",
+                value: new DateTime(2025, 10, 30, 5, 38, 37, 265, DateTimeKind.Utc).AddTicks(789));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("4286ba6a-3d40-46be-8539-237190c067b6"),
+                column: "CreatedAtUtc",
+                value: new DateTime(2025, 10, 30, 5, 38, 37, 265, DateTimeKind.Utc).AddTicks(769));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("4cf8d54f-d15c-4f02-b3a4-c40c5206b624"),
+                column: "CreatedAtUtc",
+                value: new DateTime(2025, 10, 30, 5, 38, 37, 265, DateTimeKind.Utc).AddTicks(792));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("ac8e66b0-4fbc-4c97-ad4b-427eab61db1b"),
+                column: "CreatedAtUtc",
+                value: new DateTime(2025, 10, 30, 5, 38, 37, 265, DateTimeKind.Utc).AddTicks(787));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Carts_Users_UserId",
+                table: "Carts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
