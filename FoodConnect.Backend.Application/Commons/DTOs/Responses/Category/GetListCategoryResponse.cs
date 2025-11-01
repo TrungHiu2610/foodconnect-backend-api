48f423afb2d4d316799f60a53a09e5ecf60ef3b0
TrungHiu2610 <hieult2610@gmail.com>
2025-11-01 21:31:32 +0700

    add delivery type for endpoint get categories for admin

--- FoodConnect.Backend.Application/Commons/DTOs/Responses/Category/GetListCategoryResponse.cs
+++ FoodConnect.Backend.Application/Commons/DTOs/Responses/Category/GetListCategoryResponse.cs
@@ -20,6 +20,7 @@ namespace FoodConnect.Backend.Application.Commons.DTOs.Responses.Category
         public string Name { get; set; }
         public string? Description { get; set; }
         public string? ImageUrl { get; set; }
+        public string DeliveryType { get; set; }
         public bool IsActive { get; set; }
         public Guid? ParentId { get; set; }
         public string? ParentName { get; set; }
