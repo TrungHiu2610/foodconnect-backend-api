c1335329bd1080a675ca3f62b60be5cf9e8c3b1b
TrungHiu2610 <hieult2610@gmail.com>
2025-11-01 22:20:27 +0700

    add some address fields for filling data when editting a shop

--- FoodConnect.Backend.Application/Commons/DTOs/Responses/Shop/ShopResponse.cs
+++ FoodConnect.Backend.Application/Commons/DTOs/Responses/Shop/ShopResponse.cs
@@ -5,6 +5,10 @@ namespace FoodConnect.Backend.Application.Commons.DTOs.Responses.Shop
         public Guid Id { get; set; }
         public string ShopName { get; set; } = string.Empty;
         public string? Description { get; set; }
+        public string? Street { get; set; }
+        public string? Ward { get; set; }
+        public string? District { get; set; }
+        public string? City { get; set; }
         public string Address { get; set; } = string.Empty;
         public int Status { get; set; }  
         public string StatusName { get; set; } = string.Empty;  // "Active", "Draft", etc.
